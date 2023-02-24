fa263f962111b1b6e17bf7e0b1e99327d8e54acf
Ana Maria <anamarianevesdev@gmail.com>
2023-02-24 16:45:56 -0300

    feat: implementation of two react study projects

---
+++ learning-react-router/src/Pages/Dashboard/Dashboard.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Dashboard () {
+
+
+  return (
+      <div>
+        <h1 className='text-blue-800'>Isso é um teste de rotas</h1>
+      </div>
+  )
+
+}
+
+export default Dashboard;
---
+++ learning-react-router/src/Pages/Home/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Header () {
+
+
+  return (
+      <header>
+        <h1 className='text-blue-800'>Isso é um teste de rotas</h1>
+      </header>
+  )
+
+}
+
+export default Header;
