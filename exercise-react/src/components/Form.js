41260458b7d7bc22375c67a084214f72a734184a
Ana Maria <anamarianevesdev@gmail.com>
2023-02-28 18:42:31 -0300

    fix: class about useStatess

--- exercise-react/src/components/Form.js
+++ exercise-react/src/components/Form.js
@@ -1,21 +1,33 @@
+import { useState } from "react";
+
+
 function Form() {
 
-function cadastrarUsuario(e) {
-  e.preventDefault()
-  alert(`Cadastrou o usuario `)
-}
+  function cadastrarUsuario(e) {
+    e.preventDefault()
+ 
+    alert(` Usuario cadastrado! Nome : ${name}  senha : ${password} `)
+  }
 
+
+  const [name, setName] = useState()
+  const [password, setPassword] = useState()
 
   return (
     <div>
       <h1>Meu cadastro</h1>
       <form onSubmit={cadastrarUsuario}>
         <div>
-          <input type="text"  placeholder="Digite seu nome"  />
+          <label htmlFor="name">Nome:</label>
+          <input type="text" id="name" placeholder="Digite seu nome" onChange={(e) => setName(e.target.value)} />
         </div>
         <div>
-          <input type="submit"  value="Cadastrar" />
-         </div> 
+          <label htmlFor="password" >Senha</label>
+          <input type="pssword" id="password" placeholder="Digite a sua senha"  onChange={(e) => setPassword(e.target.value)}  />
+        </div>
+        <div>
+          <input type="submit" value="Cadastrar" />
+        </div>
       </form>
     </div>
   );
