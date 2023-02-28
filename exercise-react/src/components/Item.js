7721d8c5b7021430a70d20bb8b358e86c41e39ed
Ana Maria <anamarianevesdev@gmail.com>
2023-02-28 17:05:53 -0300

    fix: class about props

--- exercise-react/src/components/Item.js
+++ exercise-react/src/components/Item.js
@@ -1,12 +1,27 @@
-function Item(props) {
+import PropsTypes from 'prop-types'
+
+function Item({ marca, ano_lancamento }) {
 
   return(
   <>
-    <li>{props.marca}</li>
-    <p>Teste</p>
+    <li>
+      {marca} - {ano_lancamento}
+    </li>
+  
 
   </>
   )
+}
+
+Item.propTypes = {
+  marca: PropsTypes.string.isRequired,
+  ano_lancamento: PropsTypes.number,
+}
+
+Item.defaultProps = {
+  marca: 'Faltou a Marca',
+  ano_lancamento: 0,
+
 }
 
 export default Item;
--- exercise-react/src/components/List.js
+++ exercise-react/src/components/List.js
@@ -7,8 +7,11 @@ function List() {
     <>
     <h1>Lista De Coisas que eu quero</h1>
    <ul>
-    <Item marca="Mercedes" />
-    <Item marca="Ford " />
+    <Item marca="Mercedes" ano_lancamento={1978} />
+    <Item marca="Ford " ano_lancamento={2010} />
+    <Item marca="Fiat" />
+    <Item marca=" " ano_lancamento={0} />
+
    </ul>
    
     </>
